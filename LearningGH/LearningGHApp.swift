c43ccf4c7d8dbbdb7a2e1bab8fd7a40a4a4fa0e1
Brendan Sullivan <bgsulliv@gmail.com>
2021-05-03 10:22:50 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Brendan Sullivan on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
